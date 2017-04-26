374d876e9cce216583900d6e0792226ea68f16c8
Minh Phung <geniusnm281995@gmail.com>
2017-04-26 10:14:12 +0700

    longlethe: add thêm 2 sensors: cảm biến cường độ ánh sáng và cảm biến lượng mưa

---
+++ sensors/LightSensor/LightSensor.ino
@@ -0,0 +1,19 @@
+#define SENSOR 10// khai báo chân digital 10 cho cảm biến
+
+#define LED 8//kháo báo chân digital 8 cho đèn LED
+
+void setup (){
+
+pinMode(LED,OUTPUT);//pinMode xuất tín hiệu đầu ra cho led
+
+pinMode(SENSOR,INPUT);//pinMode nhận tín hiệu đầu vào cho cảm biê
+
+}
+
+void loop (){
+
+int value = digitalRead(SENSOR);//lưu giá trị cảm biến vào biến value
+
+digitalWrite(LED,value);//xuất giá trị ra đèn LED
+
+}
